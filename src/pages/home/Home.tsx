c750fce48d7114f33355167f9c7062b114d7ac37
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-01-30 13:42:56 +0100

    Update meta image

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,8 +12,8 @@ export function Home() {
   return (
     <Page
       title="Layer Two Amsterdam"
-      description="Site under construction"
-      image="https://amsterdam.l2beat.com/images/under-construction.png"
+      description="Layer Two Amsterdam is a two-day technical conference dedicated to Ethereum L2 Scaling."
+      image="https://amsterdam.l2beat.com/images/meta.png"
       url="https://amsterdam.l2beat.com"
       stylesheets={['/styles/style.css']}
       scripts={['/scripts/main.js']}
