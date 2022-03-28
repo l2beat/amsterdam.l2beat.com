b0da016dbed76ac1472e1f4c7bd3bdd9d5e7e173
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-03-28 14:39:27 +0200

    Update tickets

--- src/pages/home/CtaButton.tsx
+++ src/pages/home/CtaButton.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 
 export function CtaButton() {
   return (
-    <a
-      href="/tickets"
-      className="text-white bg-brand px-4 py-2 text-xl rounded-md"
-      target="_blank"
-      rel="noopener noreferrer"
+    <span
+      className="text-white bg-brand px-4 py-2 text-xl rounded-md opacity-50 cursor-not-allowed select-none"
     >
-      Apply for a ticket
-    </a>
+      Ticket applications closed
+    </span>
   )
 }
