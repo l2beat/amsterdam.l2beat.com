ae55dcf918bd2701c9581e8cfa0bf4152a9030b4
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-04-03 16:31:33 +0200

    Add more sponsors

--- src/pages/home/CtaButton.tsx
+++ src/pages/home/CtaButton.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 export function CtaButton() {
   return (
-    <span
-      className="text-white bg-brand px-4 py-2 text-xl rounded-md opacity-50 cursor-not-allowed select-none"
-    >
+    <span className="text-white bg-brand px-4 py-2 text-xl rounded-md opacity-50 cursor-not-allowed select-none">
       Ticket applications closed
     </span>
   )
--- src/pages/home/Sponsors.tsx
+++ src/pages/home/Sponsors.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import React from 'react'
 
 import sponsorsList from '../data/sponsors.json'
@@ -18,22 +19,46 @@ export function Sponsors() {
           <SponsorItem key={i} {...sponsor} />
         ))}
       </div>
+      <h3 className="text-xl my-8">Silver Sponsors</h3>
+      <div className="grid grid-cols-6 sm:grid-cols-4 gap-2 sm:gap-4 max-w-[1000px]">
+        <div />
+        {sponsorsList.silver.map((sponsor, i) => (
+          <SponsorItem
+            key={i}
+            {...sponsor}
+            className="col-span-2 sm:col-span-1"
+          />
+        ))}
+      </div>
+      <h3 className="text-xl my-8">Party sponsor</h3>
+      <div className="grid grid-cols-4 gap-2 sm:gap-4 max-w-[1000px]">
+        {sponsorsList.party.map((sponsor, i) => (
+          <SponsorItem
+            key={i}
+            {...sponsor}
+            className="col-span-2 col-start-2"
+          />
+        ))}
+      </div>
     </Section>
   )
 }
 
 export type Sponsor = typeof sponsorsList['platinum'][0]
 
-function SponsorItem(props: Sponsor) {
+function SponsorItem(props: Sponsor & { className?: string }) {
   return (
     <a
       href={props.link}
-      className="bg-pure shadow-sm flex items-center justify-center p-2 py-4 md:p-8 rounded-md"
+      className={classNames(
+        'bg-pure shadow-sm flex items-center justify-center p-2 py-4 md:p-8 rounded-md max-h-[80px] sm:max-h-[120px] md:max-h-[180px]',
+        props.className
+      )}
     >
       <img
         src={props.image}
         alt={`${props.name}`}
-        className="hover:scale-105 transition"
+        className="hover:scale-105 transition max-h-full"
       />
     </a>
   )
