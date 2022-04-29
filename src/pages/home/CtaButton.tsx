d72c59389eb53e0969e9656639d97bd59c74498f
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-04-29 18:34:56 +0200

    Update buttons

--- src/pages/home/CtaButton.tsx
+++ src/pages/home/CtaButton.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 export function CtaButton() {
   return (
-    <a
-      href="https://streameth.tv/event/l2-amsterdam-2/"
-      className="text-white bg-brand px-4 py-2 text-xl rounded-md"
-    >
-      Watch LIVE on streameth.tv
-    </a>
+    <div className="flex gap-4">
+      <a
+        href="https://streameth.tv/event/l2-amsterdam/"
+        className="text-white bg-brand px-4 py-2 text-xl rounded-md"
+      >
+        Watch Day 1
+      </a>
+      <a
+        href="https://streameth.tv/event/l2-amsterdam-2/"
+        className="text-white bg-brand px-4 py-2 text-xl rounded-md"
+      >
+        Watch Day 2
+      </a>
+    </div>
   )
 }
